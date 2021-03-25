45b180819d0322d5199fe6481eef76c2931b6221
MonaTyagi <itsmona_2211@yahoo.com>
2021-03-25 19:27:56 +0100

    Corrected DiscoverPage and imported axios

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,13 +1,31 @@
 // import react from "react";
 import { useState } from "react";
-
+import axios from "react";
 export default function DiscoverMoviesPage() {
   const [searchText, set_searchText] = useState("");
 
-  const search = () => {
-    console.log("TODO search movies for:", searchText);
-  };
+  //   const search = () => {
+  //     console.log("TODO search movies for:", searchText);
+  //   };
+  const search = async () => {
+    console.log("Start searching for:", searchText);
 
+    // Best practice: encode the string so that special characters
+    //  like '&' and '?' don't accidentally mess up the URL
+    const queryParam = encodeURIComponent(searchText);
+
+    // Option A: use the browser-native fetch function
+    //   const data = await fetch(
+    //     `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
+    //   ).then((r) => r.json());
+
+    // Option B: use the `axios` library like we did on Tuesday
+    const data = await axios.get(
+      `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
+    );
+
+    console.log("Success!", data);
+  };
   return (
     <div>
       <h1>Discover some movies!</h1>
@@ -21,22 +39,3 @@ export default function DiscoverMoviesPage() {
     </div>
   );
 }
-const search = async () => {
-  console.log("Start searching for:", searchText);
-
-  // Best practice: encode the string so that special characters
-  //  like '&' and '?' don't accidentally mess up the URL
-  const queryParam = encodeURIComponent(searchText);
-
-  // Option A: use the browser-native fetch function
-  //   const data = await fetch(
-  //     `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
-  //   ).then((r) => r.json());
-
-  // Option B: use the `axios` library like we did on Tuesday
-  const data = await axios.get(
-    `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
-  );
-
-  console.log("Success!", data);
-};
