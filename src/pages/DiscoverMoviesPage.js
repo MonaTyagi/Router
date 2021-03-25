8a1eda0308b87e5832ddccfbfadb6926a913f00b
MonaTyagi <itsmona_2211@yahoo.com>
2021-03-25 19:21:39 +0100

    Added functionality to DiscoveryPage

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,4 +1,26 @@
 // import react from "react";
+import { useState } from "react";
+
+export default function DiscoverMoviesPage() {
+  const [searchText, set_searchText] = useState("");
+
+  const search = () => {
+    console.log("TODO search movies for:", searchText);
+  };
+
+  return (
+    <div>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={(e) => set_searchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
+    </div>
+  );
+}
 const search = async () => {
   console.log("Start searching for:", searchText);
 
