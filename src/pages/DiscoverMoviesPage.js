f509e63bfecdd48efaa30b19dd010c3ae13feb2e
MonaTyagi <itsmona_2211@yahoo.com>
2021-03-25 19:17:10 +0100

    Adding AXIOS

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,6 +1,20 @@
 // import react from "react";
-export default function DiscoverMoviesPage() {
-  <div>
-    <h1>Movies</h1>
-  </div>;
-}
+const search = async () => {
+  console.log("Start searching for:", searchText);
+
+  // Best practice: encode the string so that special characters
+  //  like '&' and '?' don't accidentally mess up the URL
+  const queryParam = encodeURIComponent(searchText);
+
+  // Option A: use the browser-native fetch function
+  //   const data = await fetch(
+  //     `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
+  //   ).then((r) => r.json());
+
+  // Option B: use the `axios` library like we did on Tuesday
+  const data = await axios.get(
+    `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
+  );
+
+  console.log("Success!", data);
+};
